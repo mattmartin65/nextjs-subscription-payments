36d0845d3b4c46dcea147966723b99b73474906a
“mattmartin65” <mattmartin65@gmail.com>
2024-07-18 13:22:15 +1000

    update 17 july

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,10 +1,17 @@
+// lib/client.js
 import { createBrowserClient } from '@supabase/ssr';
 import { Database } from '@/types_db';
 
 // Define a function to create a Supabase client for client-side operations
-export const createClient = () =>
-  createBrowserClient<Database>(
-    // Pass Supabase URL and anonymous key from the environment to the client
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+export const createClient = () => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  // Ensure Supabase URL and Anon Key are provided
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Supabase URL and Anon Key are required to create a Supabase client');
+  }
+
+  // Create and return the Supabase client
+  return createBrowserClient<Database>(supabaseUrl, supabaseAnonKey);
+}
